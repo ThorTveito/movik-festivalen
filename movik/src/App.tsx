7c6f41892a3e0578c8d6835633c99a35023ef712
Thor Tveito <thortve@stud.ntnu.no>
2026-01-27 11:33:56 +0100

    Add mobile responsive design and mobile-specific hero image

--- movik/src/App.tsx
+++ movik/src/App.tsx
@@ -1,13 +1,17 @@
 import './App.css'
 import { Banner } from './components/Banner'
 import heroImage from './assets/612715894_2370640950022369_3383395397786783488_n.jpg'
+import mobileImage from './assets/20250803_020006 (1).jpg'
 
 function App() {
   return (
     <div className="app-container">
       <Banner />
       <div className="hero-section">
-        <img src={heroImage} alt="Movik Festival" className="hero-image" />
+        <picture>
+          <source media="(max-width: 768px)" srcSet={mobileImage} />
+          <img src={heroImage} alt="Movik Festival" className="hero-image" />
+        </picture>
       </div>
     </div>
   )
